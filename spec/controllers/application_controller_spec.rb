0bde98917e5c5047d9b59dfea553ee3be9a332de
Leo Nikkilä <hello@lnikki.la>
2015-04-18 20:29:08 +0300

    Add tests for ApplicationController.

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,20 @@
+RSpec.describe ApplicationController do
+  context 'when setting up access control' do
+    it { should use_before_action(:authenticate_user!) }
+
+    it 'should use token authentication' do
+      should be_a(SimpleTokenAuthentication::TokenAuthenticationHandler)
+    end
+  end
+
+  context 'when setting up parameter filtering' do
+    it { should filter_param(:password) }
+    it { should filter_param(:authentication_token) }
+  end
+
+  context 'when setting up exception handling' do
+    it { should rescue_from(ActionController::ParameterMissing) }
+    it { should rescue_from(ActiveRecord::RecordInvalid) }
+    it { should rescue_from(ActiveRecord::RecordNotFound) }
+  end
+end
