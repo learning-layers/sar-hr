8cadfa47ff2f6f12a7e28da3ba22dd87b81762d0
Leo Nikkilä <hello@lnikki.la>
2015-05-19 09:20:48 +0300

    Add peer ID to user collection output.

--- app/serializers/user_stub_serializer.rb
+++ app/serializers/user_stub_serializer.rb
@@ -1,3 +1,4 @@
 class UserStubSerializer < ActiveModel::Serializer
-  attributes :id, :email, :name, :first_name, :last_name, :title, :status
+  attributes :id, :email, :name, :first_name, :last_name, :title, :status,
+             :peer_id
 end
