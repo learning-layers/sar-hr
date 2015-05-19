05af9732454e55a9e315327e4c54cdac00ab28a1
Leo Nikkilä <hello@lnikki.la>
2015-05-19 08:06:46 +0300

    Don't assume that a policy has a record.

--- spec/support/pundit_permit_action_matcher.rb
+++ spec/support/pundit_permit_action_matcher.rb
@@ -4,12 +4,10 @@ RSpec::Matchers.define :permit_action do |action|
   end
 
   failure_message do |policy|
-    "#{policy.class} does not permit #{action} on #{policy.record} for " +
-        "#{policy.user.inspect}."
+    "#{policy.inspect} does not permit #{action} for #{policy.user.inspect}."
   end
 
   failure_message_when_negated do |policy|
-    "#{policy.class} does not forbid #{action} on #{policy.record} for " +
-        "#{policy.user.inspect}."
+    "#{policy.inspect} does not forbid #{action} for #{policy.user.inspect}."
   end
 end
