00a4bfcc1d48e542274ba627a5054986418468a0
Leo Nikkilä <hello@lnikki.la>
2015-05-22 09:40:17 +0300

    Test CORS with multiple origins.

--- spec/api/cors_spec.rb
+++ spec/api/cors_spec.rb
@@ -7,24 +7,36 @@ RSpec.describe 'cross-origin resource sharing' do
         # Note that the origin needs to match one of the space-delimited
         # origins set by the CLIENT_ORIGINS environment variable. This variable
         # is set in rails_helper.rb for tests.
-        'Origin' => 'http://example.com',
+        'Origin' => origin,
         'Access-Control-Request-Method' => 'GET',
         'Access-Control-Request-Headers' => 'X-FOO-BAR'
       }
     end
 
-    it 'should have CORS headers' do
-      expect(response.headers.keys).to include(
-        'Access-Control-Allow-Credentials',
-        'Access-Control-Allow-Headers',
-        'Access-Control-Allow-Methods',
-        'Access-Control-Allow-Origin',
-        'Access-Control-Expose-Headers',
-        'Access-Control-Max-Age'
-      )
+    shared_examples 'successful request' do
+      it 'should have CORS headers' do
+        expect(response.headers.keys).to include(
+          'Access-Control-Allow-Credentials',
+          'Access-Control-Allow-Headers',
+          'Access-Control-Allow-Methods',
+          'Access-Control-Allow-Origin',
+          'Access-Control-Expose-Headers',
+          'Access-Control-Max-Age'
+        )
+      end
+
+      its(:status) { should eq 200 }
+      its(:body)   { should be_empty }
     end
 
-    its(:status) { should eq 200 }
-    its(:body)   { should be_empty }
+    context 'with the first allowed origin' do
+      let(:origin) { 'http://example.com' }
+      include_examples 'successful request'
+    end
+
+    context 'with the second allowed origin' do
+      let(:origin) { 'https://example.com' }
+      include_examples 'successful request'
+    end
   end
 end
