68edfc53157eb886f0cac61dcd952bf830b01cd1
Leo Nikkilä <hello@lnikki.la>
2015-06-29 16:06:04 +0300

    Improve provisioning script.
    - No longer runs as root.
    - Can now be run again for a provisioned machine.

--- Vagrantfile
+++ Vagrantfile
@@ -7,7 +7,7 @@ VAGRANTFILE_API_VERSION = '2'
 Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   config.vm.box = 'ubuntu/trusty32'
   config.vm.network 'private_network', ip: '10.11.12.13'
-  config.vm.provision 'shell', path: './provisioning/provisioning.sh'
 
-  config.ssh.shell = 'bash -c "BASH_ENV=/etc/profile exec bash"'
+  config.vm.provision 'shell', path: './provisioning/provisioning.sh',
+                               privileged: false
 end
--- provisioning/provisioning.sh
+++ provisioning/provisioning.sh
@@ -15,10 +15,10 @@ POSTGRES_CONFIG='/etc/postgresql/9.3/main/pg_hba.conf'
 RBENV_REPO='https://github.com/sstephenson/rbenv.git'
 RUBY_BUILD_REPO='https://github.com/sstephenson/ruby-build.git'
 
-RUBY_VERSION=$(cat /vagrant/.ruby-version)
+RUBY_VERSION=$(awk '{$1=$1};1' < /vagrant/.ruby-version)
 
 # Clear the log
-> ${LOG}
+> $LOG
 
 # A quiet unattended installation
 export DEBIAN_FRONTEND=noninteractive
@@ -29,74 +29,74 @@ echo "Output and errors are logged into ${LOG}."
 echo 'Configuring the system...'
 
 # Set a proper locale
-echo 'LC_ALL="en_US.UTF-8"' >> /etc/environment
+echo 'LC_ALL="en_US.UTF-8"' | sudo tee -a /etc/environment > /dev/null
 
 echo 'Upgrading the system...'
 
-apt-get -qy update &>> ${LOG}
-apt-get -qy upgrade &>> ${LOG}
+sudo apt-get -qy update &>> $LOG
+sudo apt-get -qy upgrade &>> $LOG
 
 echo 'Installing utilities...'
 
-apt-get -qy install git-core &>> ${LOG}
+sudo apt-get -qy install git-core &>> $LOG
 
 echo 'Installing Postgres...'
 
-apt-get -qy install postgresql postgresql-contrib libpq-dev &>> ${LOG}
+sudo apt-get -qy install postgresql postgresql-contrib libpq-dev &>> $LOG
 
 # Allow login from anywhere
-sed -Ei 's/local\s+all\s+postgres\s+peer/local all postgres trust/' \
-  ${POSTGRES_CONFIG}
+sudo sed -Ei \
+  's/local\s+all\s+postgres\s+peer/local all postgres trust/' $POSTGRES_CONFIG
 
-service postgresql restart &>> ${LOG}
+sudo service postgresql restart &>> $LOG
 
 echo "Installing Ruby ${RUBY_VERSION}..."
 
 # Common Ruby dependencies
-apt-get -qy install autoconf bison build-essential libssl-dev libyaml-dev \
-  libsqlite3-dev libreadline6-dev zlib1g-dev libncurses5-dev &>> ${LOG}
+sudo apt-get -qy install autoconf bison build-essential libssl-dev \
+  libyaml-dev libsqlite3-dev libreadline6-dev zlib1g-dev libncurses5-dev \
+  &>> $LOG
 
-# This provisioning script is running as root.
-# Using sudo to install rbenv as vagrant.
+if [ ! -e ~/.rbenv ]; then
+  git clone $RBENV_REPO ~/.rbenv &>> $LOG
+  git clone $RUBY_BUILD_REPO ~/.rbenv/plugins/ruby-build &>> $LOG
 
-sudo -u vagrant -H bash -c "git clone ${RBENV_REPO} ~/.rbenv" &>> ${LOG}
-sudo -u vagrant -H bash \
-  -c "git clone ${RUBY_BUILD_REPO} ~/.rbenv/plugins/ruby-build" &>> ${LOG}
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+  echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
-sudo -u vagrant -H bash \
-  -c "echo 'export PATH=\"\$HOME/.rbenv/bin:\$PATH\"' >> ~/.bash_profile"
-sudo -u vagrant -H bash \
-  -c "echo 'eval \"\$(rbenv init -)\"' >> ~/.bash_profile"
-
-sudo -u vagrant -H bash \
-  -c "~/.rbenv/bin/rbenv install ${RUBY_VERSION}" &>> ${LOG}
-sudo -u vagrant -H bash \
-  -c "~/.rbenv/bin/rbenv global ${RUBY_VERSION}" &>> ${LOG}
+  ~/.rbenv/bin/rbenv install $RUBY_VERSION &>> $LOG
+  ~/.rbenv/bin/rbenv global $RUBY_VERSION &>> $LOG
+fi
 
 echo 'Installing Bundler...'
 
-sudo -u vagrant -H bash -c '~/.rbenv/shims/gem install bundler' &>> ${LOG}
+~/.rbenv/shims/gem install bundler &>> $LOG
 
 echo 'Installing gems...'
 
-sudo -u vagrant -H bash \
-  -c '~/.rbenv/shims/bundle install --gemfile="/vagrant/Gemfile"' &>> ${LOG}
+~/.rbenv/shims/bundle install --gemfile="/vagrant/Gemfile" &>> $LOG
 
 echo 'Setting up database...'
 
-sudo -u postgres createdb development  &>> ${LOG}
-sudo -u postgres createdb test  &>> ${LOG}
+DB_ERROR='...createdb failed, skipping. Does the database exist already?'
 
-sudo -u vagrant -H bash \
-  -c '~/.rbenv/shims/rake db:setup --rakefile="/vagrant/Rakefile"' &>> ${LOG}
+sudo -u postgres createdb development &>> $LOG || echo $DB_ERROR
+sudo -u postgres createdb test &>> $LOG || echo $DB_ERROR
+
+# Set up the database if the schema exists.
+if [ -e /vagrant/db/schema.rb ]; then
+  ~/.rbenv/shims/rake db:setup --rakefile="/vagrant/Rakefile" &>> $LOG
+else
+  echo '...no schema, skipping migrations.'
+fi
 
 echo 'Starting Rails...'
 
 # Copy the Upstart job into place and refresh Upstart
-cp /vagrant/provisioning/config/rails.conf /etc/init/rails.conf &>> ${LOG}
-chmod +x /etc/init/rails.conf &>> ${LOG}
-initctl reload-configuration &>> ${LOG}
+sudo cp /vagrant/provisioning/config/rails.conf /etc/init/rails.conf &>> $LOG
+sudo chmod +x /etc/init/rails.conf &>> $LOG
+sudo initctl reload-configuration &>> $LOG
 
-service rails start &>> ${LOG}
+sudo service rails start &>> $LOG || true
 
 echo 'Done!'
